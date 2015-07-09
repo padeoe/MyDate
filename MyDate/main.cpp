b4b12f3570c9d0c8a0a5f9cc410e757f224d8b25
padeoe <padeoe@gmail.com>
2015-07-09 21:01:02 +0800

    与Java代码进行了比较

--- MyDate/main.cpp
+++ MyDate/main.cpp
@@ -2,11 +2,16 @@
 #include <iostream>
 #include "mydate.h"
 #include<time.h>
+#include<Windows.h>
 using namespace std;
 int main() {
-	MyDate d1(2015,7,9);
-	MyDate d2(1995,9,11);
-	cout << d1.day_t() << endl;
-	cout << d2.day_t()<< endl;
-	cout << d2 - d1<<endl;
+	DWORD start = GetTickCount();
+	for (int i = 2015; i < 1000000;i++) {
+		MyDate d1(i, 7, 9);
+		MyDate d2(1995, 9, 11);
+	//	d1 - d2;
+		d1.sub(d2);
+	}
+	DWORD end = GetTickCount();
+	printf("%d\n", end - start);
 }
--- MyDate/mydate.h
+++ MyDate/mydate.h
@@ -1,6 +1,8 @@
 #pragma once
+#include <iostream>
 #include <math.h>
 #include <exception>
+using namespace std;
 class MyDate{
 	int year, month, day;
 	void set(int y, int m, int d) throw(std::out_of_range);
@@ -10,27 +12,32 @@ public:
 	int maxDay() const;
 	int day_t() const;
 	friend int operator-(const MyDate& a, const MyDate& b);
+	int sub(const MyDate&a);
+	friend ostream& operator<<(ostream& o, const MyDate& a);
+	int getYear() { return year; };
+	int getMonth() { return month; };
+	int getDay() { return day; };
 };
-MyDate::MyDate(int y,int m,int d)throw(std::out_of_range) {
+inline MyDate::MyDate(int y,int m,int d)throw(std::out_of_range) {
 	set(y,m,d);
 }
 
-void MyDate::set(int y, int m, int d) throw(std::out_of_range) {
+inline void MyDate::set(int y, int m, int d) throw(std::out_of_range) {
 	if (y < 1 || m < 1 || m>12 || d<1) throw std::out_of_range("不存在该日期");
 	year = y; month = m; day = d;
 	if (day > maxDay()) throw std::out_of_range("不存在该日期");
 }
-bool MyDate::isLeapYear() const {
+inline bool MyDate::isLeapYear() const {
 	return (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0));
 }
-int MyDate::maxDay() const {
+inline int MyDate::maxDay() const {
 	int M[] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
 	if (isLeapYear() && month == 2)
 		return 29;
 	else
 		return M[month - 1];
 }
-int MyDate::day_t() const {
+inline int MyDate::day_t() const {
 	int M[] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
 	int sum = 0;
 	for (int i = 0; i < month - 1; i++)
@@ -39,6 +46,12 @@ int MyDate::day_t() const {
 		sum++;
 	return 365 * (year - 1) + (year - 1) / 4 - (year - 1) / 100 + (year - 1) / 400 + sum + day - 1;
 }
-int operator-(const MyDate& a,const MyDate& b) {
+inline int operator-(const MyDate& a,const MyDate& b) {
 	return a.day_t() - b.day_t();
+}
+inline int MyDate::sub(const MyDate& a){
+	return day_t() - a.day_t();
+}
+inline ostream& operator<<(ostream& o, const MyDate& a) {
+	return o << a.year << "/" << a.month<<"/" << a.day <<endl;
 }
